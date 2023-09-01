f3fead80bdd6878e8c02304ac10cd59a5e4cb761
devmonzer08 <142656890+devmonzer08@users.noreply.github.com>
2023-09-01 18:39:33 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+import os
+APP_ID = os.environ.get("APP_ID")
+APP_HASH = os.environ.get("APP_HASH")
+
+session1 = os.environ.get("TERMUX")
+SESSION1 = os.environ.get("TERMUX")
+DEVLOO = os.environ.get("DEVLO")
+CHNA = os.environ.get("CHNA")
+abbas = TelegramClient(StringSession(session1), APP_ID, APP_HASH)
+
+
+ispay = ['yes']
+ispay2 = ['yes']
+
+
